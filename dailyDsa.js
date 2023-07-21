dfb52940ab5464e44b884e8a9021cb1d4936892d
Sahil Verma <sahilverma15012002@gmail.com>
2023-07-21 18:04:29 +0530

    dsa 5 commmit

--- dailyDsa.js
+++ dailyDsa.js
@@ -48,4 +48,30 @@
 // let d = parseInt(prompt('Enter the second number before swapping : '));
 // [c, d] = [d, c]
 // console.log(`The swapped value of a using destructuring assignment : ${c}`);
-// console.log(`The Swapped value of a using destructuring assignment : ${d}`);                                      
+// console.log(`The Swapped value of a using destructuring assignment : ${d}`);    
+
+// Program to find the largest number among three numbers
+// function num(a,b,c) {
+//     if (a > b && a > c) {
+//         return `The largest number is ${a}`
+//     }
+//     else if (b > a && b > c) {
+//         return `The largest number is ${b}`
+//     }
+//     else if (c > a && c > b) {
+//         return `The largest number is ${c}`
+//     }
+//     else if (a == b && a > c && b > c) {
+//         return `The samllest number is ${c}`
+//     }
+//     else if (a == c && a > b && c > b) {
+//         return `The samllest number is ${b}`
+//     }
+//     else if (c == b && c > a && b > a) {
+//         return `The samllest number is ${a}`
+//     }
+//     else {
+//         return "All the Values are same"
+//     }
+// }
+console.log(num(6,7,6));

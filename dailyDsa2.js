5105087a5806cd6e11526a8579141d9a63f42c3b
Sahil Verma <sahilverma15012002@gmail.com>
2023-07-23 16:25:46 +0530

    comments off

--- dailyDsa2.js
+++ dailyDsa2.js
@@ -1,6 +1,6 @@
 // (2.) Program to Add Two Numbers
-// let input = require('prompt-sync')()
-// let num1 = parseInt(input('Enter the first number : '))
-// let num2 = parseInt(input('Enter the second number : '))
-// const Result = (num1 + num2);
-// console.log(`Addition of Two numbers is ${Result}`);
+let input = require('prompt-sync')()
+let num1 = parseInt(input('Enter the first number : '))
+let num2 = parseInt(input('Enter the second number : '))
+const Result = (num1 + num2);
+console.log(`Addition of Two numbers is ${Result}`);
--- dailyDsa6.js
+++ dailyDsa6.js
@@ -1,5 +1,5 @@
 // (6.) Program to Convert Kilometers to Miles
-// let input = require('prompt-sync')()
-// let a = parseInt(input('Enter the Km in number to change in miles : '));
-// a /= 1.609;
-// console.log(`The conversion of the given value of Km in miles is ${a}` );
+let input = require('prompt-sync')()
+let a = parseInt(input('Enter the Km in number to change in miles : '));
+a /= 1.609;
+console.log(`The conversion of the given value of Km in miles is ${a}` );
